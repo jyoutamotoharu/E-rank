b8a587751d6a756ce741ee3ccd3365a76011dc13
jyoutamotoharu <jyoka0328@gmail.com>
2020-03-27 10:35:00 +0900

    解答見本

--- C001-radio.php
+++ C001-radio.php
@@ -1,51 +1,49 @@
+<?php
+if( isset($_GET['submit'] )){
+ //採点ボタンを押していたら
+  $seikai = 0;
+  $trueAns = [0,5,8,10,14,16,20];
+  for ( $i=0; $i<7; $i++) {
+    $q=$i+1 ;//Qは1から始まるので
+    if(isset($_GET["Q$q"]) && $_GET["Q$q"] == $trueAns[$i]) 
+    $seikai++;
+  }
+  echo "あなたは",$seikai*20,"点でした！";
+}
+?>
 <form name="radioB">
-カナダの首都は？<br>
-<input type="radio" name="Q1">オタワ<br>
-<input type="radio" name="Q1">トロント<br>
-<input type="radio" name="Q1">モントリオール<br>
-<br>
-スイスの首都は？<br>
-<input type="radio" name="Q2">ジュネーブ<br>
-<input type="radio" name="Q2">チューリッヒ<br>
-<input type="radio" name="Q2">ベルン<br>
-<br>
-ドイツの首都は？<br>
-<input type="radio" name="Q3">ハンブルク<br>
-<input type="radio" name="Q3">ブレーメン<br>
-<input type="radio" name="Q3">ベルリン<br>
-<br>
-スペインの首都は？<br>
-<input type="radio" name="Q4">バルセロナ<br>
-<input type="radio" name="Q4">マドリード<br>
-<input type="radio" name="Q4">リスボン<br>
-<br>
-オーストラリアの首都は？<br>
-<input type="radio" name="Q5">シドニー<br>
-<input type="radio" name="Q5">メルボルン<br>
-<input type="radio" name="Q5">キャンベラ<br>
-<br>
-中国の首都は？<br>
-<input type="radio" name="Q6">武漢<br>
-<input type="radio" name="Q6">北京<br>
-<input type="radio" name="Q6">上海<br>
-<br>
-アメリカの首都は？<br>
-<input type="radio" name="Q7">ニューヨーク<br>
-<input type="radio" name="Q7">サンフランシスコ<br>
-<input type="radio" name="Q7">ワシントン<br>
-<br>
-<input type="button" value="採点" onclick="saiten()" />
+<?php  // "Z:\06_php\otawa\C001-radio.txt"
+$city = ["オタワ","トロント","モントリオール",
+"ジュネーブ","チューリッヒ","ベルン",
+"ハンブルク","ブレーメン","ベルリン",
+"バルセロナ","マドリード","リスボン",
+"シドニー","メルボルン","キャンベラ",
+"武漢","北京","上海",
+"ニューヨーク","サンフランシスコ","ワシントンDC"];
+$country = ["カナダ","スイス","ドイツ","スペイン","オーストラリア","中国","アメリカ"];
+// この配列をループして問題を作りたい､三回に一回は問題番号が変わる､国名も変わる 剰余演算子を使う
+$c = 0;
+foreach ($city as $key => $value) {
+  if ($key % 3 == 0) 
+    echo $country[$c++] , "の首都は？<br>";
+  echo "<input type='radio' name='Q$c' value='$key'> $value <br>";
+}
+?>
+
+
+<input type="button" value="js採点" onclick="saiten()" />
+<input type="submit" name="submit" value="php採点" />
 </form>
 
 
 
 <script>
-function saiten(){//関数への引数のリスト
+function saiten(){
   var seikai=0; //正解数を入れる変数
   //答えの番号を配列に入れる
   var trueAns = new Array(0,5,8,10,14,16,20);
   //正解のラジオボタンがチェックされているか確認
-  for (i=0 ; i<7 ; i++)  {//0~7まで1ずつ加算を繰り返す
+  for (i=0 ; i<7 ; i++)  {
     if( document.radioB.elements[trueAns[i]].checked )
       seikai++;
   }
